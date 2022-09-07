9231e909bc4e9d8ff07687399cf3add6f1989eb8
urk21cs2041 <110519141+urk21cs2041@users.noreply.github.com>
2022-09-07 08:42:20 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ circular_linked_list = list(map(int,input().strip().split(" ")))
 actual_list = []
 value = 0
 while len(actual_list) < length_of_circular_linked_list and value < len(circular_linked_list):
- element = circular_linked_list[value]
- if element not in actual_list:
- actual_list.append(element)
- value += 1
+    element = circular_linked_list[value]
+    if element not in actual_list:
+        actual_list.append(element)
+    value += 1
 print(len(actual_list))
 print(" ".join(str(num) for num in actual_list))
